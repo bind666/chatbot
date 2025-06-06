1fb0c7a95336ddb770cf7a9b9b52ff91cb8958d6
bind666 <144540630+bind666@users.noreply.github.com>
2025-06-06 13:39:22 +0530

    link updated

--- frontend/script.js
+++ frontend/script.js
@@ -11,7 +11,7 @@ async function handleUserInput() {
     showTypingIndicator(true);
 
     try {
-        const res = await fetch("http://localhost:5000/ask", {
+        const res = await fetch("https://chatbot-i968.onrender.com/ask", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ question: query }),
